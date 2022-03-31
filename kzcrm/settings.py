50305f8cf82e8d87f696b3c6e21c9680a5e3638b
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-31 09:49:47 +0500

    User make  abstract

--- kzcrm/settings.py
+++ kzcrm/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'leads.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
