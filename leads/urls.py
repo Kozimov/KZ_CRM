aad1bee622aff37dba14473343def007286b121c
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-04 16:30:58 +0500

    front files updated

--- leads/urls.py
+++ leads/urls.py
@@ -6,8 +6,8 @@ app_name = "leads"
 
 urlpatterns = [
     path('', leads_lists),
-    path('<int:pk>/detallar', lead_detail, name='detallar'),
-    path('<int:pk>/update_detail', lead_update, name='lead-update'),
-    path('<int:pk>/delete', lead_delete, name="lead-delete"),
-    path('create-yaratish/', lead_create, name="lead-create"),
+    path('<int:pk>/', lead_detail, name='detallar'),
+    path('<int:pk>/update_detail', lead_update, name='update'),
+    path('<int:pk>/delete', lead_delete, name="delete"),
+    path('create-leads/', lead_create, name="create"),
 ]
