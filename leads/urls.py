4b5dd51dafdc20aa02a56643ecf72d6c58759c31
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-04 13:12:35 +0500

    contn url

--- leads/urls.py
+++ leads/urls.py
@@ -7,7 +7,7 @@ app_name = "leads"
 urlpatterns = [
     path('', leads_lists),
     path('<int:pk>/detallar', lead_detail, name='detallar'),
-    path('<int:pk>/update_detail', lead_update),
-    path('<int:pk>/delete', lead_delete),
+    path('<int:pk>/update_detail', lead_update, name='lead-update'),
+    path('<int:pk>/delete', lead_delete, name="lead-delete"),
     path('create-yaratish/', lead_create, name="lead-create"),
 ]
