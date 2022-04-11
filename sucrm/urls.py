f250b02b3556fd484f0b85257b2a0595acd76d1b
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-11 14:37:14 +0500

    send email reset

--- sucrm/urls.py
+++ sucrm/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    )
 from django.urls import path, include
 from leads.views import HomeView, SigupView
 
@@ -11,5 +16,7 @@ urlpatterns = [
     path('agents/', include('agents.urls', namespace="agents")),
     path('signup/', SigupView.as_view(), name="signup"),
     path('login/', LoginView.as_view(), name="login"),
+    path('password-reset-view/', PasswordResetView.as_view(), name="password_reset_view"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('logout/', LogoutView.as_view(), name="logout")
 ]
