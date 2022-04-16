408a7dc714de2165f9b12e61371f02ca88985189
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-16 09:15:45 +0500

    Category models

--- leads/admin.py
+++ leads/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Lead, User, Agent, UserProfile
+from .models import Lead, User, Agent, UserProfile, Category
 
 admin.site.register(Lead)
+admin.site.register(Category)
 admin.site.register(User)
 admin.site.register(UserProfile)
 admin.site.register(Agent)
--- leads/models.py
+++ leads/models.py
@@ -19,9 +19,17 @@ class Lead(models.Model):
     yoshi = models.IntegerField(default=0)
     organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return str(self.familiyasi)
+
+class Category(models.Model):
+    nomi = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.nomi)
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
